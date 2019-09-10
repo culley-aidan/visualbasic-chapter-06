f859e7e8d9de56e08eebf46a7a549106301a8dba
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-10 10:21:50 -0500

    changed size

--- Fitness Challenge/Fitness Challenge/Form1.Designer.vb
+++ Fitness Challenge/Fitness Challenge/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class frmFitness
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(867, 438)
         Me.Name = "frmFitness"
         Me.Text = "Fitness Challenge"
         Me.ResumeLayout(False)
