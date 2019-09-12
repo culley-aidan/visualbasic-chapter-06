d743892b20d0116b6b3b61d2db7d21b6293df8fb
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-12 08:38:59 -0500

    declaring variables

--- Fitness Challenge/Fitness Challenge/Form1.vb
+++ Fitness Challenge/Fitness Challenge/Form1.vb
@@ -9,6 +9,26 @@
 Option Strict On
 Public Class frmFitness
     Private Sub BtnWeightLoss_Click(sender As Object, e As EventArgs) Handles btnWeightLoss.Click
+        ' The btnWeightLoss_Click event accepts and displays up to 8 weight loss values
+        ' and then calculates and displays the average weight loss for the team
+
+        ' Declaring variables
+
+        '   Weight values
+        Dim strWeightLoss As String
+        Dim decWeightLoss As Decimal
+        Dim decAverageLoss As Decimal
+        Dim decTotalWeightLoss As Decimal = 0D
+
+        '   Information strings
+        Dim strInputMessage As String = "Enter the weight loss for team member #"
+        Dim strInputHeading As String = "Weight Loss"
+        Dim strNormalMessage As String = "Enter the weight loss for team member #"
+
+        '   Errors
+        Dim strNonNumericError As String = "Error - Enter a number for the weight loss of team member #"
+        Dim strNegativeError As String = "Error - Enter a positive number for the weight loss of team member #"
+
 
     End Sub
 End Class
