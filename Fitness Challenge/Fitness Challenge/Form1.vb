e9a7dfa596157f61bf6003ecaad1b902d04f5228
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-12 08:34:24 -0500

    began working on the code side

--- Fitness Challenge/Fitness Challenge/Form1.vb
+++ Fitness Challenge/Fitness Challenge/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class frmFitness
+﻿' Program Name: Fitness Challenge
+' Author:       Aidan J Culley
+' Date:         September 12th, 2019
+' Purpose:      The Fitness Challenge program enteres the weight loss
+'               loss from the team members for a fitness challenge. It displays each
+'               weight loss value. After all weight loss values have been entered, 
+'               it displays the average weight loss for the team.
 
+Public Class frmFitness
+    Private Sub BtnWeightLoss_Click(sender As Object, e As EventArgs) Handles btnWeightLoss.Click
+
+    End Sub
 End Class
