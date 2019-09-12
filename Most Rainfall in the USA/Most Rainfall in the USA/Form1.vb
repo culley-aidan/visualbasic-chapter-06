b73c82b5f9c6438b9d17681d332066a77a18dc06
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-12 09:05:20 -0500

    started Most rainfall in the usa

---
+++ Most Rainfall in the USA/Most Rainfall in the USA/Form1.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmRainfall
+
+End Class
---
+++ Most Rainfall in the USA/Most Rainfall in the USA/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Most Rainfall in the USA")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Most Rainfall in the USA")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d5f9cdfe-917a-49a8-979c-cbde0d2b216f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
