1786c5b831c90b2e72f37c3e1ca853344fc9faf5
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-12 09:08:32 -0500

    created mnuClear

--- Most Rainfall in the USA/Most Rainfall in the USA/Form1.Designer.vb
+++ Most Rainfall in the USA/Most Rainfall in the USA/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class frmRainfall
     Private Sub InitializeComponent()
         Me.mnuRainfall = New System.Windows.Forms.MenuStrip()
         Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuRainfall.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -38,9 +39,16 @@ Partial Class frmRainfall
         '
         'mnuFile
         '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuClear})
         Me.mnuFile.Name = "mnuFile"
         Me.mnuFile.Size = New System.Drawing.Size(37, 20)
         Me.mnuFile.Text = "&File"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(180, 22)
+        Me.mnuClear.Text = "&Clear"
         '
         'frmRainfall
         '
@@ -60,4 +68,5 @@ Partial Class frmRainfall
 
     Friend WithEvents mnuRainfall As MenuStrip
     Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
 End Class
