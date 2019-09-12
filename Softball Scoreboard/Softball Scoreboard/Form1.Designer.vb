db108ae0bb5bb058046f6764335f133fe336ab04
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-12 09:53:54 -0500

    created mnuClear

--- Softball Scoreboard/Softball Scoreboard/Form1.Designer.vb
+++ Softball Scoreboard/Softball Scoreboard/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class frmSoftball
     Private Sub InitializeComponent()
         Me.mnuScoreboard = New System.Windows.Forms.MenuStrip()
         Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
         Me.mnuScoreboard.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -38,9 +39,16 @@ Partial Class frmSoftball
         '
         'mnuFile
         '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuClear})
         Me.mnuFile.Name = "mnuFile"
         Me.mnuFile.Size = New System.Drawing.Size(37, 20)
         Me.mnuFile.Text = "&File"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(180, 22)
+        Me.mnuClear.Text = "&Clear"
         '
         'frmSoftball
         '
@@ -60,4 +68,5 @@ Partial Class frmSoftball
 
     Friend WithEvents mnuScoreboard As MenuStrip
     Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
 End Class
