fc670faced5987f2b5f1fc4ea3dbfef310ba46b6
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-13 08:56:51 -0500

    started coding

--- Factorial Calculator/Factorial Calculator/Form1.vb
+++ Factorial Calculator/Factorial Calculator/Form1.vb
@@ -1,3 +1,8 @@
-﻿Public Class frmFactorial
+﻿' Program Name: Factorial Calculator
+' Author:       Aidan J Culley
+' Date:         September 12th, 2019
+' Purpose:      calculates factorials
+
+Public Class frmFactorial
 
 End Class
