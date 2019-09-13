dd3d0f76ab16aa69c180cb45614f698b9ab30867
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-13 08:23:07 -0500

    started coding

--- Softball Scoreboard/Softball Scoreboard/Form1.Designer.vb
+++ Softball Scoreboard/Softball Scoreboard/Form1.Designer.vb
@@ -38,7 +38,7 @@ Partial Class frmSoftball
         Me.mnuScoreboard.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile})
         Me.mnuScoreboard.Location = New System.Drawing.Point(0, 0)
         Me.mnuScoreboard.Name = "mnuScoreboard"
-        Me.mnuScoreboard.Size = New System.Drawing.Size(800, 24)
+        Me.mnuScoreboard.Size = New System.Drawing.Size(375, 24)
         Me.mnuScoreboard.TabIndex = 0
         Me.mnuScoreboard.Text = "MenuStrip1"
         '
@@ -52,13 +52,13 @@ Partial Class frmSoftball
         'mnuClear
         '
         Me.mnuClear.Name = "mnuClear"
-        Me.mnuClear.Size = New System.Drawing.Size(180, 22)
+        Me.mnuClear.Size = New System.Drawing.Size(101, 22)
         Me.mnuClear.Text = "&Clear"
         '
         'mnuExit
         '
         Me.mnuExit.Name = "mnuExit"
-        Me.mnuExit.Size = New System.Drawing.Size(180, 22)
+        Me.mnuExit.Size = New System.Drawing.Size(101, 22)
         Me.mnuExit.Text = "E&xit"
         '
         'lblTitle
@@ -75,7 +75,7 @@ Partial Class frmSoftball
         '
         Me.lblFinalScore.AutoSize = True
         Me.lblFinalScore.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblFinalScore.Location = New System.Drawing.Point(14, 362)
+        Me.lblFinalScore.Location = New System.Drawing.Point(42, 322)
         Me.lblFinalScore.Name = "lblFinalScore"
         Me.lblFinalScore.Size = New System.Drawing.Size(276, 29)
         Me.lblFinalScore.TabIndex = 2
@@ -84,7 +84,7 @@ Partial Class frmSoftball
         'btnInnings
         '
         Me.btnInnings.Font = New System.Drawing.Font("Tahoma", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnInnings.Location = New System.Drawing.Point(97, 93)
+        Me.btnInnings.Location = New System.Drawing.Point(98, 79)
         Me.btnInnings.Name = "btnInnings"
         Me.btnInnings.Size = New System.Drawing.Size(167, 38)
         Me.btnInnings.TabIndex = 3
@@ -96,17 +96,17 @@ Partial Class frmSoftball
         Me.lstInnings.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lstInnings.FormattingEnabled = True
         Me.lstInnings.ItemHeight = 19
-        Me.lstInnings.Location = New System.Drawing.Point(110, 161)
+        Me.lstInnings.Location = New System.Drawing.Point(144, 142)
         Me.lstInnings.Name = "lstInnings"
         Me.lstInnings.RightToLeft = System.Windows.Forms.RightToLeft.Yes
-        Me.lstInnings.Size = New System.Drawing.Size(120, 137)
+        Me.lstInnings.Size = New System.Drawing.Size(62, 137)
         Me.lstInnings.TabIndex = 4
         '
         'frmSoftball
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(375, 391)
         Me.Controls.Add(Me.lstInnings)
         Me.Controls.Add(Me.btnInnings)
         Me.Controls.Add(Me.lblFinalScore)
--- Softball Scoreboard/Softball Scoreboard/Form1.vb
+++ Softball Scoreboard/Softball Scoreboard/Form1.vb
@@ -1,3 +1,9 @@
-﻿Public Class frmSoftball
+﻿' Program Name: Softball Scoreboard
+' Author:       Aidan J Culley
+' Date:         September 12th, 2019
+' Purpose:      Calculates innings score no idea how this works
+
+
+Public Class frmSoftball
 
 End Class
