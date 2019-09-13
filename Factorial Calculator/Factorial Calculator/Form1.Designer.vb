5634fbf35d017bcfff1e810fdb3be3574430f5c4
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-13 08:47:43 -0500

    created lblTitle

--- Factorial Calculator/Factorial Calculator/Form1.Designer.vb
+++ Factorial Calculator/Factorial Calculator/Form1.Designer.vb
@@ -22,17 +22,31 @@ Partial Class frmFactorial
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Tahoma", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(188, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(361, 42)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Factorial Calculator"
         '
         'frmFactorial
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblTitle)
         Me.Name = "frmFactorial"
         Me.Text = "Factorial Calculator"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblTitle As Label
 End Class
